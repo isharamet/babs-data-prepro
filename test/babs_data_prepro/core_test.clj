d71e2e1029572b3ed2322fec8eebc639fae1ca63
Ivan Sharamet <isharamet@gmail.com>
2017-06-13 13:26:19 +0300

    Delete 'test' dir

--- test/babs_data_prepro/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns babs-data-prepro.core-test
-  (:require [clojure.test :refer :all]
-            [babs-data-prepro.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
