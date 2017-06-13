993befe4eb093bccfdc94b8744829f3e0e68317d
Ivan Sharamet <isharamet@gmail.com>
2017-06-13 12:06:34 +0300

    Add simple preprocessing implementation

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [me.raynes/fs "1.4.6"]])
+                 [me.raynes/fs "1.4.6"]
+                 [clj-time "0.13.0"]]
+  :main babs-data-prepro.core)
--- src/babs_data_prepro/core.clj
+++ src/babs_data_prepro/core.clj
@@ -1,6 +1,64 @@
-(ns babs-data-prepro.core)
+(ns babs-data-prepro.core
+  (:require [clojure.string :as s]
+            [me.raynes.fs :as fs]
+            [clj-time.format :as f]
+            [clojure.string :as str])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def default-formatter (f/formatter "yyyy-MM-dd"))
+
+(def files
+  [{:file-mask #".+_station_data.csv" :file-type "station-data" :date-pos 6 :date-format "MM/dd/yyyy"}
+   {:file-mask #".+_status_data.csv" :file-type "status-data" :date-pos 3 :date-format "yyyy-MM-dd HH:mm:ss"}
+   {:file-mask #".+_trip_data.csv" :file-type "trip-data" :date-pos 2 :date-format "yyyy/MM/dd HH:mm:ss"}
+   {:file-mask #".+_weather_data.csv" :file-type "weather-data" :date-pos 0 :date-format "MM/dd/yyyy"}])
+
+(defn write-file
+  [lines file-type file-date]
+  (let [dir (str "resources/" file-type)
+        file (str dir "/" file-date ".csv")]
+    (do
+      (if (not (fs/exists? dir))
+        (fs/mkdir dir))
+      (with-open [wrtr (clojure.java.io/writer file :append true)]
+        (doseq [line lines]
+          (.write wrtr line)
+          (.newLine wrtr))))))
+
+(defn strip-quotes
+  [s]
+  (str/replace s "\"" ""))
+
+(defn process-file
+  [file-path file-info]
+  (let [{:keys [file-type date-pos date-format]} file-info
+        formatter (f/formatter date-format)]
+    (with-open [rdr (clojure.java.io/reader file-path)]
+      (reduce
+        (fn [acc line]
+          (do
+            (println line)
+            (let [lines (:lines acc)
+                  prev-date (:prev-date acc)
+                  xs (s/split line #",")
+                  date (f/unparse default-formatter
+                                  (f/parse formatter
+                                           (strip-quotes (nth xs date-pos))))]
+              (if (and (not (nil? prev-date)) (not= prev-date date))
+                (do
+                  (write-file lines file-type prev-date)
+                  {:lines [line] :prev-date date})
+                {:lines (conj lines line) :prev-date date}))))
+        {:lines [] :prev-date nil}
+        (rest (line-seq rdr))))))
+
+(defn walk-files
+  [dir]
+  (doseq [file (fs/list-dir dir)
+          file-info files]
+    (if (re-matches (:file-mask file-info) (.getName file))
+      (process-file (.getPath file) file-info))))
+
+(defn -main
+  [dir & args]
+  (walk-files dir))
